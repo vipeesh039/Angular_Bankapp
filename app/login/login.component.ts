16c6b2b6cd5a6e0fbddecebfc768b4d7ee753d0b
prajeesh <prajeesh039@gmail.com>
2021-04-28 19:31:48 +0530

    logincomponent__update view of login

--- app/login/login.component.ts
+++ app/login/login.component.ts
@@ -8,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   aim="!!"
-   account_details = {
+
+  accnum=""
+  pswd=""
+
+   account_details :any = {
     1000: { name: "ajay", accno: 1000, password: "testone", amount: 5000 },
     1001: { name: "vijay", accno: 1001, password: "testtwo", amount: 3000 },
     1002: { name: "ram", accno: 1002, password: "testthree", amount: 7000 },
@@ -21,8 +25,36 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  login(){
-    alert("Login clicked");
+  accnochange(event:any){
+    this.accnum=event.target.value;
+    console.log(this.accnum)
+  }
+  pwdChange(event:any){
+    this.pswd=event.target.value;
+    console.log(this.pswd)
+
   }
 
+  login(){
+    alert("Login clicked");
+    var accno=this.accnum
+    var pwd=this.pswd
+
+    let details =this.account_details;
+        if (accno in details) {
+            if (pwd == details[accno]["password"]) {
+                alert("login success");
+            }
+            else {
+                alert("incorrect password");
+            }
+        }
+        else {
+            alert("invalid account number");
+        }
+      
+  }
+ 
 }
+
+
